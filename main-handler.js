1079d4a745d6ddf41c48c1d3c9c77dc1802a04e9
Adam Jarrell <adam.jarrell@gmail.com>
2017-10-11 13:29:31 -0400

    Added button styles, changed ajax request to pull URI and subsequent selector from array of objects

---
+++ main-handler.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var ratesArray = [
+	{ site: "Site 1", selector: "table#rates-chart", uri: "https://americanstoragenc.com/self-storage/pittsboro-west-nc-27312" },
+	{ site: "H and R", selector: "table#rates-chart", uri: "https://www.excessstoragenc.com/self-storage-knightdale-hr.php" },
+	{ site: "Harrisburg", selector: "table#rates-chart", uri: "https://www.bestharrisburgselfstorage.com/self-storage/harrisburg-nc-28075" },
+	{ site: "The Attic", selector: "table#rates-chart", uri: "https://theatticstoragenc.com/self-storage-rates.php" },
+	{ site: "Site 2", selector: "table#rates-chart", uri: "https://americanstoragenc.com/self-storage/pittsboro-east-nc-27312" },
+	{ site: "Smithfield", selector: "table#rates-chart", uri: "https://www.excessstoragenc.com/self-storage-knightdale-smithfield.php" },
+	{ site: "Speedway", selector: "table.units-table", uri: "https://www.bestcarolinastorage.com/self-storage-harrisburg-nc-91762" },
+	{ site: "AAA", selector: "table#rates-chart", uri: "https://durhamstoragesolutions.com/self-storage/durham-nc-27703" }
+];
+
+$(document).ready(function(){
+	
+	$('#pricing').click(getRates);
+	
+});
+
+function getRates(){
+	$(ratesArray).each(function(i){
+
+		$.get(ratesArray[i].uri, [], function(d){
+
+			var h = $(d);
+
+			$(document.createElement('div'))
+				.addClass('cell')
+				.append(h.find(ratesArray[i].selector))
+				.prepend(ratesArray[i].site)
+				.appendTo('#target');
+
+
+		}, "html");
+	});
+}
