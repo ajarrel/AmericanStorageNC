5a2b3f2ee0f5220e56ee8ceacad5b9b4eab5d373
Adam Jarrell <adam.jarrell@gmail.com>
2017-10-16 09:49:39 -0400

    Added popup.html with a button to open main.html through UI. Version incremented to 0.103. Began implementation of sparefoot rates piping into page.

--- main-handler.js
+++ main-handler.js
@@ -11,14 +11,14 @@ var ratesArray = [
 	{ site: 'AAA', order: 7, selector: 'table#rates-chart', uri: 'https://durhamstoragesolutions.com/self-storage/durham-nc-27703' }
 ];
 var sparefoot = [ //selector is div.unit-list, stored in index 0.selector
-	{ site: 'Site 1', order: 0, uri: 'https://americanstoragenc.com/self-storage/pittsboro-west-nc-27312', selector: 'div.unit-list' },
+	{ site: 'Site 1', order: 0, uri: 'https://www.sparefoot.com/Pittsboro-NC-self-storage/American-Self-Storage-Highway-64-155481', selector: 'div.unit-list' },
 	{ site: 'H and R', order: 3, uri: 'https://www.sparefoot.com/Knightdale-NC-self-storage/Excess-Storage-Center-109843' },
-	{ site: 'Harrisburg', order: 4, uri: 'https://www.bestharrisburgselfstorage.com/self-storage/harrisburg-nc-28075' },
-	{ site: 'The Attic', order: 6, uri: 'https://theatticstoragenc.com/self-storage-rates.php' },
-	{ site: 'Site 2', order: 1, uri: 'https://americanstoragenc.com/self-storage/pittsboro-east-nc-27312' },
-	{ site: 'Smithfield', order: 2, uri: 'https://www.excessstoragenc.com/self-storage-knightdale-smithfield.php' },
-	{ site: 'Speedway', order: 5, uri: 'https://www.bestcarolinastorage.com/self-storage-harrisburg-nc-91762' },
-	{ site: 'AAA', order: 7, uri: 'https://durhamstoragesolutions.com/self-storage/durham-nc-27703' }
+	{ site: 'Harrisburg', order: 4, uri: 'https://www.sparefoot.com/Harrisburg-NC-self-storage/Harrisburg-Self-Storage-152501' },
+	{ site: 'The Attic', order: 6, uri: 'https://www.sparefoot.com/Concord-NC-self-storage/The-Attic-Self-Storage-199468' },
+	{ site: 'Site 2', order: 1, uri: 'https://www.sparefoot.com/Pittsboro-NC-self-storage/American-Self-Storage-Mt-Gilead-109842' },
+	{ site: 'Smithfield', order: 2, uri: 'https://www.sparefoot.com/Knightdale-NC-self-storage/Excess-Storage-Center-Smithfield-Road-199467' },
+	{ site: 'Speedway', order: 5, uri: 'https://www.sparefoot.com/Harrisburg-NC-self-storage/Speedway-Self-Storage-153068' },
+	{ site: 'AAA', order: 7, uri: 'https://www.sparefoot.com/Durham-NC-self-storage/AAA-Mini-Storage-202242' }
 ];
 
 var promises = [];
@@ -31,10 +31,12 @@ $(document).ready(function(){
 	});
 	
 	$('#pricing').click(getRates);
+	$('#sparefoot').click(getRates);
 	
 });
 
-function getRates(){
+function getRates(e){
+	console.log(e);
 	$(ratesArray).each(function(i){
 
 		var deferred = new $.Deferred();
