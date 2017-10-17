bb37b7f4dcd412fe3ebea8330fa97c66ffd3fd1a
Adam Jarrell <adam.jarrell@gmail.com>
2017-10-17 17:46:33 -0400

    Modified style.css to support Sparefoot content, callback for array[0].callback. Modified popup.html with a better button UI, default page width. Known issues: .clicked style not working (not sticky, reverts to default style). Version incremented by 1 minor version number.

--- main-handler.js
+++ main-handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var ratesArray = [
+var ratesTable = [
 	{ site: 'Site 1', order: 0, selector: 'table#rates-chart', uri: 'https://americanstoragenc.com/self-storage/pittsboro-west-nc-27312' },
 	{ site: 'H and R', order: 3, selector: 'table#rates-chart', uri: 'https://www.excessstoragenc.com/self-storage-knightdale-hr.php' },
 	{ site: 'Harrisburg', order: 4, selector: 'table#rates-chart', uri: 'https://www.bestharrisburgselfstorage.com/self-storage/harrisburg-nc-28075' },
@@ -11,17 +11,23 @@ var ratesArray = [
 	{ site: 'AAA', order: 7, selector: 'table#rates-chart', uri: 'https://durhamstoragesolutions.com/self-storage/durham-nc-27703' }
 ];
 var sparefoot = [ //selector is div.unit-list, stored in index 0.selector
-	{ site: 'Site 1', order: 0, uri: 'https://www.sparefoot.com/Pittsboro-NC-self-storage/American-Self-Storage-Highway-64-155481', selector: 'div.unit-list' },
-	{ site: 'H and R', order: 3, uri: 'https://www.sparefoot.com/Knightdale-NC-self-storage/Excess-Storage-Center-109843' },
-	{ site: 'Harrisburg', order: 4, uri: 'https://www.sparefoot.com/Harrisburg-NC-self-storage/Harrisburg-Self-Storage-152501' },
-	{ site: 'The Attic', order: 6, uri: 'https://www.sparefoot.com/Concord-NC-self-storage/The-Attic-Self-Storage-199468' },
-	{ site: 'Site 2', order: 1, uri: 'https://www.sparefoot.com/Pittsboro-NC-self-storage/American-Self-Storage-Mt-Gilead-109842' },
-	{ site: 'Smithfield', order: 2, uri: 'https://www.sparefoot.com/Knightdale-NC-self-storage/Excess-Storage-Center-Smithfield-Road-199467' },
-	{ site: 'Speedway', order: 5, uri: 'https://www.sparefoot.com/Harrisburg-NC-self-storage/Speedway-Self-Storage-153068' },
-	{ site: 'AAA', order: 7, uri: 'https://www.sparefoot.com/Durham-NC-self-storage/AAA-Mini-Storage-202242' }
+	{ 
+		site: 'Site 1', order: 0, uri: 'https://www.sparefoot.com/Pittsboro-NC-self-storage/American-Self-Storage-Highway-64-155481', selector: 'div.unit-list',
+		callback: function(){
+			$('button[type="button"] > *').unwrap();
+			$('span.button-content').filter(function(){ return ($(this).text() === 'Continue') }).remove();
+		}
+	},
+	{ site: 'H and R', order: 3, uri: 'https://www.sparefoot.com/Knightdale-NC-self-storage/Excess-Storage-Center-109843', selector: 'div.unit-list' },
+	{ site: 'Harrisburg', order: 4, uri: 'https://www.sparefoot.com/Harrisburg-NC-self-storage/Harrisburg-Self-Storage-152501', selector: 'div.unit-list' },
+	{ site: 'The Attic', order: 6, uri: 'https://www.sparefoot.com/Concord-NC-self-storage/The-Attic-Self-Storage-199468', selector: 'div.unit-list' },
+	{ site: 'Site 2', order: 1, uri: 'https://www.sparefoot.com/Pittsboro-NC-self-storage/American-Self-Storage-Mt-Gilead-109842', selector: 'div.unit-list' },
+	{ site: 'Smithfield', order: 2, uri: 'https://www.sparefoot.com/Knightdale-NC-self-storage/Excess-Storage-Center-Smithfield-Road-199467', selector: 'div.unit-list' },
+	{ site: 'Speedway', order: 5, uri: 'https://www.sparefoot.com/Harrisburg-NC-self-storage/Speedway-Self-Storage-153068', selector: 'div.unit-list' },
+	{ site: 'AAA', order: 7, uri: 'https://www.sparefoot.com/Durham-NC-self-storage/AAA-Mini-Storage-202242', selector: 'div.unit-list' }
 ];
 
-var promises = [];
+var promises = [],global;
 
 
 $(document).ready(function(){
@@ -30,26 +36,30 @@ $(document).ready(function(){
 		$('button').not(this).removeClass('clicked');
 	});
 	
-	$('#pricing').click(getRates);
-	$('#sparefoot').click(getRates);
+	$('#pricing').click(ratesTable, getRemoteData);
+	$('#sparefoot').click(sparefoot, getRemoteData);
 	
 });
 
-function getRates(e){
+function getRemoteData(e){
 	console.log(e);
+	var ratesArray = e.data;
+	
+	$('#target').empty();
+	
 	$(ratesArray).each(function(i){
 
 		var deferred = new $.Deferred();
-		$.get(ratesArray[i].uri, [], function(d){
+		$.get(e.data[i].uri, [], function(d){
 
 			var h = $( d.replace(/<img\b[^>]*>/ig, '') ); //strip images out of response before wrapping in jQuery
 			h.find('td.rate-button').remove();
 			
-			var wrapper = $(document.createElement('h1')).attr('id',ratesArray[i].site).text(ratesArray[i].site);
+			var wrapper = $(document.createElement('h1')).attr('id',e.data[i].site).text(e.data[i].site);
 
-			ratesArray[i].dom = $(document.createElement('div'))
+			e.data[i].dom = $(document.createElement('div'))
 				.addClass('cell')
-				.append(h.find(ratesArray[i].selector))
+				.append(h.find(e.data[i].selector))
 				.prepend(wrapper);
 			
 			deferred.resolve();
@@ -60,19 +70,25 @@ function getRates(e){
 		
 	});
 	
-	$.when.apply(undefined, promises).promise().done(buildDom);
+	$.when.apply(undefined, promises).promise().done(function(){
+		buildDom(e.data);
+	});
 }
 
-function buildDom(){
-	var tArr = new Array(ratesArray.length), order; //tie the length of this temp array to the ratesArray
-	
-	$(ratesArray).each(function(i){
-		order = ratesArray[i].order;
-		
-		tArr[order] = ratesArray[i].dom;
+function buildDom(obj){
+	console.log("Obj inside buildDom"); console.log(obj);
+	var tArr = new Array(obj.length), order; //tie the length of this temp array to the ratesArray
+	global = obj;
+	$(obj).each(function(i){
+		order = obj[i].order;
+		tArr[order] = obj[i].dom;
 	});
 	
 	$(tArr).each(function(){
 		$(this).appendTo('#target');
 	});
+	
+	if (obj[0].callback !== undefined){ //optional callback, if exists, call it AFTER appending DOM
+		obj[0].callback();
+	}
 }
